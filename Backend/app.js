943b4eef7d3dab6ac035e78aa2c1ba868366ba79
Biswadip125 <162881134+Biswadip125@users.noreply.github.com>
2025-07-22 16:23:14 +0530

    Updated Frontend url in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://watch-flix-ten.vercel.app");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, PUT, POST, DELETE, UPDATE, OPTIONS "
